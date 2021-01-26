18e8000e5e602b9f53f627b95dd4c1a693b12fcb
eoho1 <eoho1@DESKTOP-LU08BOG>
2021-01-27 02:20:37 +0900

    fix calendar

--- src/honux/calandar/Calendar.java
+++ src/honux/calandar/Calendar.java
@@ -3,8 +3,8 @@ package honux.calandar;
 public class Calendar {
 
 	public static void main(String[] args) {
-	System.out.println("ÀÏ  ¿ù  È­  ¼ö    ¸ñ   ±İ   Åä");
-	System.out.println("1   2   3  4    5   6    7");
+	System.out.println("ì¼  ì›”   í™”   ìˆ˜   ëª©   ê¸ˆ   í† ");
+	System.out.println("1   2   3   4    5   6    7");
 	System.out.println("8   9   10  11  12  13  14");
 	System.out.println("15  16  17  18  19  20  21");
 	System.out.println("22  23  24  25  26  27  28");
